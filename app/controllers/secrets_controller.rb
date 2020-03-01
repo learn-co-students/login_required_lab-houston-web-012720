61b464be8b2257128068112f01023a863cb7f4ec
somaia-khalil <sumaya_khair@live.com>
2020-03-01 10:20:39 -0600

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,15 @@
+class SecretsController < ApplicationController
+
+    before_action :require_login, only:[:show]
+
+
+    def show 
+    
+    end 
+
+
+    def require_login
+        redirect_to login_path  unless session.include? :name
+    end 
+end
+
